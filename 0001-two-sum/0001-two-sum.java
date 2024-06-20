948c790ec056bb63d2fc5a6a724aaac6bcb100ac
Deepak Chauhan <143954019+dc-hub0@users.noreply.github.com>
2024-06-20 20:14:46 +0530

    Time: 2 ms (97.79%), Space: 45.1 MB (6.94%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+               
+                return new int[] { map.get(complement), i };
+            }
+           
+            map.put(nums[i], i);
+        }
+        
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        // Test cases
+        Solution solution = new Solution();
+        
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Output: [" + result1[0] + "," + result1[1] + "]");
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        System.out.println("Output: [" + result2[0] + "," + result2[1] + "]");
+
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = solution.twoSum(nums3, target3);
+        System.out.println("Output: [" + result3[0] + "," + result3[1] + "]");
+    }
+}
