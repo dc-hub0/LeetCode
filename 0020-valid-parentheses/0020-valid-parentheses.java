1833d53a7ecfc14a6d9555a6c5318e09ddd87dc5
Deepak Chauhan <143954019+dc-hub0@users.noreply.github.com>
2024-06-25 20:50:59 +0530

    Time: 1 ms (98.93%), Space: 41.6 MB (26.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.isValid("()")); // true
+        System.out.println(solution.isValid("()[]{}")); // true
+        System.out.println(solution.isValid("(]")); // false
+    }
+}
