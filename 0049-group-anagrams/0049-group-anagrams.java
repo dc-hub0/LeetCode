28a4f69ba966ae2c0c2af7e0300ebf4586fff20f
Deepak Chauhan <143954019+dc-hub0@users.noreply.github.com>
2024-06-28 18:49:05 +0530

    Time: 7 ms (69.84%), Space: 48 MB (28.41%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+   
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String str : strs) {
+     
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+ 
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+     
+            map.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
