4ae239510b18266ebb4d3591b2951ccaa8d8ee0c
Deepak Chauhan <143954019+dc-hub0@users.noreply.github.com>
2024-07-04 18:46:33 +0530

    Time: 1 ms (100%), Space: 41.7 MB (96.27%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+        
+        if (carry != 0) result.append(carry);
+        return result.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.addBinary("11", "1")); // "100"
+        System.out.println(solution.addBinary("1010", "1011")); // "10101"
+    }
+}
