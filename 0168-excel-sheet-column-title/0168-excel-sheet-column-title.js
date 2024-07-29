8bbbfe8d5650f1ed38d1e4b2f55661bc49ddd014
Deepak Chauhan <143954019+dc-hub0@users.noreply.github.com>
2024-07-29 20:22:07 +0530

    Time: 38 ms (97.01%), Space: 48.1 MB (92.25%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+    let result = '';
+    
+    while (columnNumber > 0) {
+        columnNumber--; 
+        let remainder = columnNumber % 26;
+        result = String.fromCharCode(remainder + 65) + result;
+        columnNumber = Math.floor(columnNumber / 26);
+    }
+    
+    return result;
+};
+
+console.log(convertToTitle(1));  
+console.log(convertToTitle(28));  
+console.log(convertToTitle(701)); 
