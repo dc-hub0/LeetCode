cf8097803f903c4650ba097826229fff2c344561
Deepak Chauhan <143954019+dc-hub0@users.noreply.github.com>
2024-09-03 19:44:36 +0530

    Time: 64 ms (30.33%), Space: 50.9 MB (52.78%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,11 @@
+function reverseBits(n) {
+    let result = 0;
+    for (let i = 0; i < 32; i++) {
+        result = (result << 1) | (n & 1);
+        n >>>= 1;
+    }
+    return result >>> 0;
+}
+
+console.log(reverseBits(43261596));  
+console.log(reverseBits(4294967293));  
